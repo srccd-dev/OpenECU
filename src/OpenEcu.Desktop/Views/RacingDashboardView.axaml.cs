12fcd2e0e3e55aac9b295d687a9ae8d8f182e770
Michael Neumann <michael.c.neumann1@gmail.com>
2026-06-10 21:08:01 -0500

    feat: RacingDashboardView + Standard/Racing toggle

---
+++ src/OpenEcu.Desktop/Views/RacingDashboardView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OpenEcu.Desktop.Views;
+
+public partial class RacingDashboardView : UserControl
+{
+    public RacingDashboardView() => AvaloniaXamlLoader.Load(this);
+}
